c1ca4a33d8bd13162d8cef7492720d3888ac99fb
nshumoogum <nathan.shumoogum@methods.co.uk>
2017-06-22 07:27:34 +0100

    Extend elastic search script
    1) Search handles multiple flags to load data sets from different
       csv files.
    2) Extend README.md
    3) Create config/template files to set mappings for indexing.

--- elastic/main.go
+++ elastic/main.go
@@ -12,11 +12,18 @@ import (
 	"gopkg.in/olivere/elastic.v5"
 )
 
+var (
+	esDestURL   = "http://127.0.0.1:9200"
+	esDestIndex = "v3data"
+	esDestType  = "test1"
+)
+
 func main() {
 	var docType, csvFile string
-
-	flag.StringVar(&csvFile, "f", "", "A V3 csv file")
-	flag.StringVar(&docType, "t", "", "type name")
+	flag.StringVar(&esDestURL, "es-dest-url", esDestURL, "elasticsearch destination URL")
+	flag.StringVar(&esDestIndex, "es-dest-index", esDestIndex, "elasticsearch destination index")
+	flag.StringVar(&esDestType, "es-dest-type", esDestType, "elasticsearch destination type")
+	flag.StringVar(&csvFile, "file-location", "", "A V3 csv file")
 	flag.Parse()
 
 	file, err := os.Open(csvFile)
@@ -24,12 +31,12 @@ func main() {
 		log.Panic(err)
 	}
 	searchClient, _ := elastic.NewClient(
-		elastic.SetURL("http://127.0.0.1:9200"),
+		elastic.SetURL(esDestURL),
 		elastic.SetMaxRetries(5),
 		elastic.SetSniff(false))
 	bulk, _ := searchClient.BulkProcessor().
-		BulkSize(15000).
-		Workers(32).
+		BulkSize(30000).
+		Workers(8).
 		FlushInterval(time.Millisecond * 1000).
 		Do(context.Background())
 	bulk.Start(context.Background())
@@ -37,6 +44,7 @@ func main() {
 	csvReader := csv.NewReader(file)
 	skipFirstRow := false
 	rowCount := 0
+	var lengthOfTime time.Duration
 	var csvHeaders []string
 	startTime := time.Now()
 	for {
@@ -53,19 +61,25 @@ func main() {
 				docs[csvHeaders[i]] = record[i]
 			}
 			request := elastic.NewBulkIndexRequest().
-				Index("v3data").
-				Type(docType).
+				Index(esDestIndex).
+				Type(esDestType).
 				Doc(docs)
+
+			startTimeOfRequest := time.Now()
 			bulk.Add(request)
+			endTimeOfRequest := time.Now()
+			durationOfRequest := endTimeOfRequest.Sub(startTimeOfRequest)
 			rowCount = rowCount + 1
+			lengthOfTime = lengthOfTime + durationOfRequest
 		} else {
 			skipFirstRow = true
 			csvHeaders = make([]string, len(record))
 			copy(csvHeaders, record)
 		}
 	}
+
 	bulk.Flush()
 	endTime := time.Now()
 	duration := endTime.Sub(startTime)
-	log.Printf("Completed, Rows %d, Time %d ns - %f s", rowCount, duration.Nanoseconds(), duration.Seconds())
+	log.Printf("Completed, Rows %d, Time %d ns - %f s \nLength of all requests is : %vs", rowCount, duration.Nanoseconds(), duration.Seconds(), lengthOfTime.Seconds())
 }
